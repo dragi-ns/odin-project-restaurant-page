8999b02c575098fe880952011ee5da2d75c86b26
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2021-12-10 20:29:39 +0100

    Update project structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,57 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist'
+  mode: 'development', // production
+  entry: {
+    main: './src/js/index.js',
   },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].bundle.js',
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    devMiddleware: {
+      writeToDisk: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[contenthash][ext]'
+        }
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin()
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      inject: 'body'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css',
+      ignoreOrder: false
+    })
+  ]
 };
